767fc7252981fdaf362cca970e308a34fcbdf97b
Pack Heng <pack@oz-heng.com>
2017-09-12 08:01:03 +1000

    Renamed AlbumsListActivity and albums_list_main.xmls to CatalogueActivity and activity_catalogue.xml.  On branch 001_AlbumsListActivity  Changes to be committed: 	modified:   .idea/misc.xml 	new file:   .idea/vcs.xml 	modified:   app/src/main/AndroidManifest.xml 	renamed:    app/src/main/java/com/oz_heng/apps/android/musicshop/AlbumsListActivity.java -> app/src/main/java/com/oz_heng/apps/android/musicshop/CatalogueActivity.java 	renamed:    app/src/main/res/layout/albums_list_main.xml -> app/src/main/res/layout/activity_catalogue.xml

--- app/src/main/java/com/oz_heng/apps/android/musicshop/CatalogueActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/musicshop/CatalogueActivity.java
@@ -3,11 +3,11 @@ package com.oz_heng.apps.android.musicshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AlbumsListActivity extends AppCompatActivity {
+public class CatalogueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.albums_list_main);
+        setContentView(R.layout.activity_catalogue);
     }
 }
